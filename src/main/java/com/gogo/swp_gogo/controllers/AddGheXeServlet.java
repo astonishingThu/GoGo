2a6780b252a43b150089cb8085c4b74f93c2e5e1
Gem-bnx <baongoc.bnx@gmail.com>
2023-06-09 10:22:47 +0700

    BN0906_UpdateThemXe2

---
+++ src/main/java/com/gogo/swp_gogo/controllers/AddGheXeServlet.java
@@ -0,0 +1,27 @@
+package com.gogo.swp_gogo.controllers;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name="addGheXeServlet", value = "/AddGheXe")
+
+public class AddGheXeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        String[] vipList = req.getRequ("vipList");
+        String[] vipList = new String[]{req.getParameter("vipList")};
+        for(int i =0 ; i<vipList.length; i++){
+            System.out.println(vipList[i]);
+        }
+    }
+}
